e6c0acc7b8c00eab01e0892bcbd83cbed31278ec
mathraphael <mthsrphl@gmail.co m>
2024-01-19 02:55:35 -0400

    [NOVO] #0 - MENU MOBILE FUNCIONANDO

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,48 @@
+const nav = document.querySelector(".navbar-second");
+const btnMenu = document.querySelector(".btn-menu");
+const menu = document.querySelector(".nav-list");
 
-const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(event){
-    if(event.type == 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+
+function handleButtonClick(event){
+
+    if(event.type === "touchstart") event.preventDefault();
+    event.stopPropagation();
+    nav.classList.toggle("active");
+    handleClickOutside(menu, () =>{
+        nav.classList.remove("active");
+        setAria();
+    });
+    setAria();
 }
 
+function handleClickOutside(targetElement, callback){
+    const html = document.documentElement;
+    function handleHTMLClick(event){
+        if(!targetElement.contains(event.target)){
+            targetElement.removeAttribute("data-target");
+            html.removeEventListener("click", handleHTMLClick);
+            html.removeEventListener("touchstart", handleHTMLClick);
+            callback();
+        }
+    }
+    if(!targetElement.hasAttribute("data-target")){
+        html.addEventListener("click", handleHTMLClick);
+        html.addEventListener("touchstart", handleHTMLClick);
+        targetElement.setAttribute("data-target","");
+    }
 
-btnMobile.addEventListener('click',toggleMenu);
-btnMobile.addEventListener('touchstart',toggleMenu, {passive:true});
+}
 
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
+function setAria(){
+    const isActive = nav.classList.contains("active");
+    btnMenu.setAttribute("aria-expanded", isActive);
+    if(isActive){
+        btnMenu.setAttribute("aria-label", "Fechar Menu");
+    }else{
+        btnMenu.setAttribute("aria-label", "Abrir Menu");
+    }
+}
 
-btnScrollToTop.addEventListener("click", function(){
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
-    });
-
+btnMenu.addEventListener("click", handleButtonClick);
+btnMenu.addEventListener("touchstart", handleButtonClick);
