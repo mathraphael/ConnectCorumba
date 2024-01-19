5ca7d1476acca81aeaed34352b8a03c757626a40
mathraphael <mthsrphl@gmail.co m>
2024-01-19 03:27:23 -0400

    [CORRECAO] #0 - Tudo Funfando

--- public/js/app.js
+++ public/js/app.js
@@ -46,3 +46,13 @@ function setAria(){
 
 btnMenu.addEventListener("click", handleButtonClick);
 btnMenu.addEventListener("touchstart", handleButtonClick);
+
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+    });
