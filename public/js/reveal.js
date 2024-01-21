c11a6cd6eaea81df673a7302f418f3548f8daf16
mathraphael <mthsrphl@gmail.co m>
2024-01-20 23:41:27 -0400

    [NOVO] #0 - Aplicando Animacao com o Scroll Reveal

---
+++ public/js/reveal.js
@@ -0,0 +1,100 @@
+
+window.revelar = ScrollReveal({reset: true});
+
+//Este mes
+revelar.reveal('.efeito-card1',{
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card2',{
+    duration: 2000,
+    delay: 500,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card3',{
+    duration: 2000,
+    delay: 1000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card4',{
+    duration: 2000,
+    delay: 1500,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card5',{
+    duration: 2000,
+    delay: 2000,
+    distance: '90px'
+
+});
+
+revelar.reveal('.efeito-card6',{
+    duration: 2000,
+    delay: 2500,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card7',{
+    duration: 2000,
+    delay: 3000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-card8',{
+    duration: 2000,
+    delay: 3500,
+    distance: '90px'
+});
+
+//conteudo
+revelar.reveal('.efeito-content1',{
+    duration: 2000,
+    delay:500,
+    
+});
+
+revelar.reveal('.efeito-content2',{
+    duration: 2000,
+    delay:1000,
+    
+});
+
+revelar.reveal('.efeito-content3',{
+    duration: 2000,
+    delay:1500,
+    
+});
+
+revelar.reveal('.efeito-content4',{
+    duration: 2000,
+    delay:2000,
+    
+});
+
+revelar.reveal('.efeito-content5',{
+    duration: 2000,
+    delay:2500,
+    
+});
+
+revelar.reveal('.efeito-content6',{
+    duration: 2000,
+    delay:3000,
+    
+});
+
+revelar.reveal('.efeito-content7',{
+    duration: 2000,
+    delay:3500,
+    
+});
+
+revelar.reveal('.efeito-content8',{
+    duration: 2000,
+    delay:4000,
+    
+});
